49d502cffddf713890a4249e3856006bf94adb7b
Bruno Totoli <santosbruno101062@gmail.com>
2022-06-11 18:09:15 -0300

    H2-Database config

---
+++ src/test/java/bruno/api/rest/springrestapi/SpringrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package bruno.api.rest.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
