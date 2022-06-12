8f52c731f81a20e275991be0aa8b8356c4bf7315
Bruno Totoli <santosbruno101062@gmail.com>
2022-06-11 21:00:56 -0300

    PostMapping User

--- src/main/java/bruno/api/rest/springrestapi/controllers/UserController.java
+++ src/main/java/bruno/api/rest/springrestapi/controllers/UserController.java
@@ -3,11 +3,9 @@ package bruno.api.rest.springrestapi.controllers;
 import bruno.api.rest.springrestapi.entities.User;
 import bruno.api.rest.springrestapi.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,11 @@ public class UserController {
     @GetMapping(value = "/")
     public ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @PostMapping(value = "/")
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
 }
